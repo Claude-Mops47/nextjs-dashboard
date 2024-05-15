634aa2c3003038863fec6e46856960f1037590bd
Claude Emmanuel Mopeno-Bia <mopenobiaclaude@gmail.com>
2024-05-15 10:35:21 +0100

    Pagination

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,11 +2,11 @@ import SideNav from '../ui/dashboard/sidenav';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex-co flex h-screen md:flex-row md:overflow-hidden">
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="md:p-4 flex-grow p-6 md:overflow-y-auto">{children}</div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   );
 }
--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -13,8 +13,8 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const currentPage = Number(searchParams.get('page')) || 1;
 
   const createPageURL = (pageNumber: number | string) => {
-    const params = new URLSearchParams(searchParams);
-    params.set('page', pathname.toString());
+    const params = new URLSearchParams(searchParams); 
+    params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   };
 
